6ced476fdab13139cb936b99225e54fa5cf7538a
Damien <damienambient@gmail.com>
2024-11-09 00:40:19 +0200

    BTN design created

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import { Heading } from "./components/heading"
 import { MaxWidthWrapper } from "./components/max-width-wrapper"
+import { Check } from "lucide-react"
+import { ShinyButton } from "./components/shiny-button"
 
 const Page = () => {
   return (
@@ -8,7 +10,38 @@ const Page = () => {
         <MaxWidthWrapper className="text-center">
           <div className="relative mx-auto text-center flex flex-col items-center gap-10">
             <div>
-              <Heading>Real-Time Saas Insights,</Heading>
+              <Heading>
+                <span>Real-Time Saas Insights,</span>
+                <br />
+                <span className="relative bg-gradient-to-r from-brand-700 to-brand-800 text-transparent bg-clip-text">
+                  Delivered to Your discord
+                </span>
+              </Heading>
+            </div>
+            <p className=" text-base/7 text-gray-600 max-w-prose text-center text-pretty">
+              PingPanda is the easiest way to monitor your SaaS. Get instant
+              notifications for{" "}
+              <span className="font-semibold text-gray-700">
+                sales, new users, or any other event
+              </span>{" "}
+              sent directly to your Discord.
+            </p>
+
+            <ul className=" space-y-2 text-base/7 text-gray-600 text-left flex flex-col sm:items-start">
+              {[
+                "Real-time Discord alerts for critical events",
+                "Buy once, use forever",
+                "Track sales, new users, or any other event",
+              ].map((item, index) => (
+                <li key={index} className="flex gap-1.5 items-center text-left">
+                  <Check className="size-5 shrink-0 text-brand-700"></Check>
+                  {item}
+                </li>
+              ))}
+            </ul>
+
+            <div className="w-full max-w-80">
+              <ShinyButton></ShinyButton>
             </div>
           </div>
         </MaxWidthWrapper>
