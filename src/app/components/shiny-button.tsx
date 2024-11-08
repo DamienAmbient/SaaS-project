e8ea195ac03b50088f2d429c3f0ced1e5b33017a
Damien <damienambient@gmail.com>
2024-11-09 00:54:05 +0200

    Button finished

--- src/app/components/shiny-button.tsx
+++ src/app/components/shiny-button.tsx
@@ -1,4 +1,5 @@
 import { cn } from "@/utils"
+import { ArrowRight } from "lucide-react"
 import Link from "next/link"
 import { AnchorHTMLAttributes } from "react"
 
@@ -19,7 +20,11 @@ export const ShinyButton = ({
       )}
       {...props}
     >
-      BTN
+      <span className="relative z-10 flex items-center gap-2">
+        {children}
+        <ArrowRight className="size-4 shrink-0 text-white transition-transform duration-300 ease-in-out group-hover:translate-x-[2px]"></ArrowRight>
+      </span>
+      <div className="ease-[cubic-bezier(0.19,1,0.22,1)] absolute -left-[75px] -top-[50px] -z-10 h-[155px] w-8 rotate-[35deg] bg-white opacity-20 transition-all duration-500 group-hover:left-[120%]"></div>
     </Link>
   )
 }
