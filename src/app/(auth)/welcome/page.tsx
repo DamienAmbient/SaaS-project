1764996cac37c9863b6e195f0c0d7ed005b0e5ac
Damien <damienambient@gmail.com>
2024-11-23 18:42:14 +0200

    first router

--- src/app/(auth)/welcome/page.tsx
+++ src/app/(auth)/welcome/page.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 // synchro auth status to database
 
 import { client } from "@/app/lib/client"
@@ -5,13 +7,25 @@ import { Heading } from "@/components/heading"
 import { LoadingSpinner } from "@/components/loading-spinner"
 import { useQuery } from "@tanstack/react-query"
 import { LucideProps } from "lucide-react"
+import { useRouter } from "next/navigation"
+import { useEffect } from "react"
 
 const Page = () => {
-  const {} = useQuery({
-    queryFn: () => {
-      client
+  const router = useRouter()
+  const { data } = useQuery({
+    queryFn: async () => {
+      const res = await client.auth.getDatabaseSyncStatus.$get()
+      return await res.json()
+    },
+    queryKey: ["get-database-sync-status"],
+    refetchInterval: (query) => {
+      return query.state.data?.isSynced ? false : 1000
     },
   })
+
+  useEffect(() => {
+    if (data?.isSynced) router.push("/dashboard")
+  }, [data, router])
 
   return (
     <div className="flex w-full flex-1 items-center justify-center px-4">
