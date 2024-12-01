ddcda6a03f2b58b4aca184e7f2b6df7fce3a6518
Damien <damienambient@gmail.com>
2024-12-02 00:41:59 +0200

    dashboard empty state page completed

--- src/components/create-event-category-modal.tsx
+++ src/components/create-event-category-modal.tsx
@@ -50,7 +50,14 @@ const EMOJI_OPTIONS = [
   { emoji: "🔔", label: "Notification" },
 ]
 
-export const CreateEventCategoryModal = ({ children }: PropsWithChildren) => {
+interface CreateEventCategoryModal extends PropsWithChildren {
+  containerClassName?: string
+}
+
+export const CreateEventCategoryModal = ({
+  children,
+  containerClassName,
+}: CreateEventCategoryModal) => {
   const [isOpen, setIsOpen] = useState(false)
   const queryClient = useQueryClient()
 
@@ -84,6 +91,7 @@ export const CreateEventCategoryModal = ({ children }: PropsWithChildren) => {
   return (
     <>
       <div
+        className={containerClassName}
         onClick={() => {
           setIsOpen(true)
         }}
--- src/server/routers/category-router.ts
+++ src/server/routers/category-router.ts
@@ -5,6 +5,7 @@ import { startOfMonth } from "date-fns"
 import { z } from "zod"
 import { CATEGORY_NAME_VALIDATOR } from "@/app/lib/validators/category-validator"
 import { parseColor } from "@/utils"
+import { count } from "console"
 
 export const categoryRouter = router({
   getEventCategories: privateProcedure.query(async ({ c, ctx }) => {
@@ -108,4 +109,18 @@ export const categoryRouter = router({
 
       return c.json({ eventCategory })
     }),
+
+  insertQuickstartCategories: privateProcedure.mutation(async ({ ctx, c }) => {
+    const categories = await db.eventCategory.createMany({
+      data: [
+        { name: "Bug", emoji: "🐛", color: 0xff6b6b },
+        { name: "Sale", emoji: "💰", color: 0xffeb3b },
+        { name: "Question", emoji: "❓", color: 0x6c5ce7 },
+      ].map((category) => ({
+        ...category,
+        userId: ctx.user.id,
+      })),
+    })
+    return c.json({ success: true, count: categories.count })
+  }),
 })
