5a7feeb411c84eb4cabde4fa0a741c64b104f2ad
Damien <damienambient@gmail.com>
2024-12-12 22:54:21 +0200

    Events table started

--- src/app/dashboard/category/[name]/category-page-content.tsx
+++ src/app/dashboard/category/[name]/category-page-content.tsx
@@ -6,6 +6,9 @@ import { EmptyCategoryState } from "./empty-category-state"
 import { useState } from "react"
 import { useSearchParams } from "next/navigation"
 import { client } from "@/app/lib/client"
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+import { Card } from "@/components/ui/card"
+import { BarChart } from "lucide-react"
 
 interface CategoryPageContentProps {
   hasEvents: boolean
@@ -62,4 +65,32 @@ export const CategoryPageContent = ({
     refetchOnWindowFocus: false,
     enabled: pollingData.hasEvents,
   })
+
+  return (
+    <div className="space-y-6">
+      <Tabs
+        value={activeTab}
+        onValueChange={(value) => {
+          setActiveTab(value as "today" | "week" | "month")
+        }}
+      >
+        <TabsList className="mb-2">
+          <TabsTrigger value="today">Today</TabsTrigger>
+          <TabsTrigger value="week">This Week</TabsTrigger>
+          <TabsTrigger value="month">This Month</TabsTrigger>
+        </TabsList>
+
+        <TabsContent value={activeTab}>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 mb-16">
+            <Card className="border-2 border-brand-700">
+              <div className="flex flex-row items-center justify-between space-y-0 pb-2">
+                <p className="text-sm/6 font-medium">Total Events</p>
+                <BarChart className="size-4 text-muted-foreground"></BarChart>
+              </div>
+            </Card>
+          </div>
+        </TabsContent>
+      </Tabs>
+    </div>
+  )
 }
