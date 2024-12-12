5a0d307bc55de0bcd46fe3aa80da0e180a6b488a
Damien <damienambient@gmail.com>
2024-12-12 23:43:19 +0200

    calc for sale events finished

--- src/app/dashboard/category/[name]/category-page-content.tsx
+++ src/app/dashboard/category/[name]/category-page-content.tsx
@@ -3,12 +3,13 @@
 import { EventCategory } from "@prisma/client"
 import { useQuery } from "@tanstack/react-query"
 import { EmptyCategoryState } from "./empty-category-state"
-import { useState } from "react"
+import { useMemo, useState } from "react"
 import { useSearchParams } from "next/navigation"
 import { client } from "@/app/lib/client"
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import { Card } from "@/components/ui/card"
 import { BarChart } from "lucide-react"
+import { isAfter, isToday, startOfMonth, startOfWeek } from "date-fns"
 
 interface CategoryPageContentProps {
   hasEvents: boolean
@@ -66,6 +67,58 @@ export const CategoryPageContent = ({
     enabled: pollingData.hasEvents,
   })
 
+  const numericFieldSums = useMemo(() => {
+    if (!data?.events || data.events.length === 0) return {}
+
+    const sums: Record<
+      string,
+      {
+        total: number
+        thisWeek: number
+        thisMonth: number
+        today: number
+      }
+    > = {}
+
+    const now = new Date()
+    const weekStart = startOfWeek(now, { weekStartsOn: 0 })
+    const monthStart = startOfMonth(now)
+
+    data.events.forEach((event) => {
+      const eventDate = event.createdAt
+
+      Object.entries(event.fields as object).forEach(([field, value]) => {
+        if (typeof value === "number") {
+          if (!sums[field]) {
+            sums[field] = { total: 0, thisWeek: 0, thisMonth: 0, today: 0 }
+          }
+
+          sums[field].total += value
+
+          if (
+            isAfter(eventDate, weekStart) ||
+            eventDate.getTime() === weekStart.getTime()
+          ) {
+            sums[field].thisWeek += value
+          }
+
+          if (
+            isAfter(eventDate, monthStart) ||
+            eventDate.getTime() === monthStart.getTime()
+          ) {
+            sums[field].thisMonth += value
+          }
+
+          if (isToday(eventDate)) {
+            sums[field].today += value
+          }
+        }
+      })
+    })
+
+    return sums
+  }, [data?.events])
+
   return (
     <div className="space-y-6">
       <Tabs
@@ -86,6 +139,18 @@ export const CategoryPageContent = ({
               <div className="flex flex-row items-center justify-between space-y-0 pb-2">
                 <p className="text-sm/6 font-medium">Total Events</p>
                 <BarChart className="size-4 text-muted-foreground"></BarChart>
+              </div>
+
+              <div>
+                <p className="text-2xl font-bold">{data?.eventsCount || 0}</p>
+                <p className="text-xs/5 text-muted-foreground">
+                  Events{" "}
+                  {activeTab === "today"
+                    ? "today"
+                    : activeTab === "week"
+                    ? "this week"
+                    : "this month"}
+                </p>
               </div>
             </Card>
           </div>
